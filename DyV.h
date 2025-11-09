be12b0e9122ea42d7e6d77e6e1e01ea9388cacce
Enric <enricxz@gmail.com>
2025-11-09 23:45:24 +0100

    Añadido QuickSort y medición de tiempos en el test

--- DyV.h
+++ DyV.h
@@ -75,4 +75,27 @@ int binarySearchRecursiveINV(const std::vector<T>& arr, const T& e, int izq, int
     }
 }
 
+template <typename T>
+T Particion(std::vector<T>& arr, int ini, int fin) {
+    T pivote = arr[fin];
+    int i = ini - 1;
+    for (int j = ini; j < fin; j++) {
+        if (arr[j] <= pivote) {
+            i++;
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    std::swap(arr[i + 1], arr[fin]);
+    return i + 1;
+}
+
+template <typename T>
+void quickSort(std::vector<T>& arr, int ini, int fin) {
+    if (ini < fin) {
+        T pivote = Particion(arr, ini, fin);
+        quickSort(arr, ini, pivote - 1);
+        quickSort(arr, pivote + 1, fin);
+    }
+}
+
 #endif
--- test_DyV.cpp
+++ test_DyV.cpp
@@ -1,6 +1,7 @@
 #include "DyV.h"
 #include <iostream>
 #include <vector>
+#include<chrono>
 
 int main() {
     // Creamos tres tipos de vectores diferentes para probar las funciones
@@ -43,6 +44,29 @@ int main() {
     std::cout << "Busqueda binaria no recursiva invertida con int: " << indexInt << std::endl;
     std::cout << "Busqueda binaria no recursiva invertida con double: " << indexDouble << std::endl;
     std::cout << "Busqueda binaria no recursiva invertida con char: " << indexChar << std::endl;
+
+    // Añadimos vectores int, double y char desordenados para probar el quickSort
+    std::vector<int> vecIntUnsorted = {15, 3, 9, 1, 7, 11, 5, 13};
+    std::vector<double> vecDoubleUnsorted = {4.5, 1.5, 5.5, 0.5, 3.5, 2.5};
+    std::vector<char> vecCharUnsorted = {'m', 'c', 'i', 'a', 'g', 'e', 'k'};
+
+    auto start = std::chrono::system_clock::now();
+    quickSort(vecIntUnsorted, 0, vecIntUnsorted.size() - 1);
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> duracion = end - start;
+    std::cout << "Tiempo de quickSort con int: " << duracion.count() << "s\n";
+
+    start = std::chrono::system_clock::now();
+    quickSort(vecDoubleUnsorted, 0, vecDoubleUnsorted.size() - 1);
+    end = std::chrono::system_clock::now();
+    duracion = end - start;
+    std::cout << "Tiempo de quickSort con double: " << duracion.count() << "s\n";
+
+    start = std::chrono::system_clock::now();
+    quickSort(vecCharUnsorted, 0, vecCharUnsorted.size() - 1);
+    end = std::chrono::system_clock::now();
+    duracion = end - start;
+    std::cout << "Tiempo de quickSort con char: " << duracion.count() << "s\n";
 
     return 0;
 }
